b26d14aa7a4c63fb5ab750b4312ac87b4b2faab4
Sergey Doroshenko <sedorosh@microsoft.com>
2017-10-13 17:00:47 -0700

    test1

---
+++ touchLibrary1/touchLibrary1.h
@@ -0,0 +1,19 @@
+//
+//  touchLibrary1.h
+//  touchLibrary1
+//
+//  Created by Sergey Doroshenko on 10/13/17.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for touchLibrary1.
+FOUNDATION_EXPORT double touchLibrary1VersionNumber;
+
+//! Project version string for touchLibrary1.
+FOUNDATION_EXPORT const unsigned char touchLibrary1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <touchLibrary1/PublicHeader.h>
+
+
