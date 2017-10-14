2dec3e2eef45b45eb84cf15e7e7e6bde6e645875
Sergey Doroshenko <sedorosh@microsoft.com>
2017-10-13 17:13:56 -0700

    fix1

---
+++ touchLibrary1/Classes/SDViewController.h
@@ -0,0 +1,15 @@
+//
+//  SDViewController.h
+//  touchLibrary1
+//
+//  Created by Sergey Doroshenko on 10/13/17.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDViewController : UIViewController
+
+@property (nonatomic, strong) NSString *test1;
+
+@end
---
+++ touchLibrary1/Classes/SDViewController.m
@@ -0,0 +1,37 @@
+//
+//  SDViewController.m
+//  touchLibrary1
+//
+//  Created by Sergey Doroshenko on 10/13/17.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+#import "SDViewController.h"
+
+@interface SDViewController ()
+
+@end
+
+@implementation SDViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
